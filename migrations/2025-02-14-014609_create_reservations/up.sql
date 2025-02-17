15cc298096384ab5cc2efe7fa5cd6b901bf751f8
Henrique Moraes <ahmdesouza@gmail.com>
2025-02-16 21:53:04 -0300

    update

--- migrations/2025-02-14-014609_create_reservations/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE reservations (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    client_id UUID NOT NULL REFERENCES clients(id) ON DELETE CASCADE,
-    service TEXT NOT NULL,
-    appointment_time TIMESTAMP NOT NULL,
-    status TEXT NOT NULL DEFAULT 'pending'
-);
