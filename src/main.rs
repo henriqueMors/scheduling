3e3b975545eb353b53540097a0ba83f06ae5d76c
Henrique Moraes <ahmdesouza@gmail.com>
2025-02-16 23:15:09 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}", addr);
-    Server::bind(&addr)
+    Server::try_bind(&addr)
+        .expect("Failed to bind address")
         .serve(app.into_make_service())
         .await
         .unwrap();
