ecf09102cd7eae8afc31bcfe0ec8a7b17c8ebf45
Henrique Moraes <ahmdesouza@gmail.com>
2025-02-27 21:15:24 -0300

    update migrations

--- migrations/2025-02-28-001403_create_admins/up.sql
+++ migrations/2025-02-28-001403_create_admins/up.sql
@@ -1,1 +1,7 @@
--- Your SQL goes here
+CREATE TABLE admins (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    master_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
