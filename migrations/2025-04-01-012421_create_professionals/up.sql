6427fd4aa0f354c5b90da5d2b008ef44a9183b85
Henrique Moraes <ahmdesouza@gmail.com>
2025-03-31 22:26:01 -0300

    Update up.sql

--- migrations/2025-04-01-012421_create_professionals/up.sql
+++ migrations/2025-04-01-012421_create_professionals/up.sql
@@ -1,1 +1,7 @@
--- Your SQL goes here
+CREATE TABLE professionals (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+    bio TEXT,
+    specialties TEXT[] DEFAULT '{}',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
