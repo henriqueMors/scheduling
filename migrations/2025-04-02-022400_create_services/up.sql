b10173bb88ab4795434d9146955f9c46eff2b349
Henrique Moraes <ahmdesouza@gmail.com>
2025-04-08 22:51:07 -0300

    udpate

--- migrations/2025-04-02-022400_create_services/up.sql
+++ migrations/2025-04-02-022400_create_services/up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE services (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL,
-    description TEXT,
-    price NUMERIC NOT NULL
+    nome TEXT NOT NULL,                  -- Alterado para nome
+    descricao TEXT,                      -- Alterado para descricao
+    preco DOUBLE PRECISION NOT NULL,     -- Alterado para DOUBLE PRECISION
+    duracao_min INTEGER NOT NULL,        -- Novo campo para duração em minutos
+    ativo BOOLEAN NOT NULL DEFAULT TRUE  -- Novo campo para indicar se está ativo
 );
