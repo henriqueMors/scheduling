056fc9e6dc03af8cb23e13af3324e87730161b49
Henrique Moraes <ahmdesouza@gmail.com>
2025-04-21 16:53:50 -0300

    Update availability.rs

--- src/models/availability.rs
+++ src/models/availability.rs
@@ -1,32 +1,33 @@
 use diesel::{Queryable, Insertable, Identifiable, AsChangeset};
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
-use chrono::{NaiveDate, NaiveTime};  // Importando corretamente os tipos NaiveDate e NaiveTime
+use chrono::{NaiveDate, NaiveTime};
 use crate::schema::availabilities;
+use diesel::dsl::date;
 
 #[derive(Debug, Queryable, Serialize, Deserialize, Identifiable)]
 #[diesel(table_name = availabilities)]
 pub struct Availability {
     pub id: Uuid,
     pub professional_id: Uuid,
-    pub date: NaiveDate,          // Usando NaiveDate corretamente para a data
-    pub start_time: NaiveTime,    // Usando NaiveTime para o horário de início
-    pub end_time: NaiveTime,      // Usando NaiveTime para o horário de término
+    pub date: NaiveDate,      
+    pub start_time: NaiveTime,
+    pub end_time: NaiveTime,   
 }
 
 #[derive(Debug, Insertable, Deserialize)]
 #[diesel(table_name = availabilities)]
 pub struct NewAvailability {
     pub professional_id: Uuid,
-    pub date: NaiveDate,          // Usando NaiveDate corretamente para a data
-    pub start_time: NaiveTime,    // Usando NaiveTime para o horário de início
-    pub end_time: NaiveTime,      // Usando NaiveTime para o horário de término
+    pub date: NaiveDate,      
+    pub start_time: NaiveTime,
+    pub end_time: NaiveTime,   
 }
 
 #[derive(Debug, AsChangeset, Deserialize)]
 #[diesel(table_name = availabilities)]
 pub struct UpdateAvailability {
-    pub date: Option<NaiveDate>,  // Usando NaiveDate corretamente como tipo opcional
+    pub date: Option<NaiveDate>,
     pub start_time: Option<NaiveTime>,
     pub end_time: Option<NaiveTime>,
 }
