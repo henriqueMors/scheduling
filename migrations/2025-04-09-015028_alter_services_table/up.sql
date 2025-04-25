12134631d70a85f83167b1a51f66b70c15ce7329
Henrique Moraes <ahmdesouza@gmail.com>
2025-04-25 00:06:35 -0300

    update

--- migrations/2025-04-09-015028_alter_services_table/up.sql
+++
@@ -1,9 +0,0 @@
--- Renomear a coluna 'price' para 'preco'
-ALTER TABLE services RENAME COLUMN price TO preco;
-
--- Adiciona as novas colunas (duracao_min e ativo)
-ALTER TABLE services ADD COLUMN duracao_min INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE services ADD COLUMN ativo BOOLEAN NOT NULL DEFAULT TRUE;
-
--- Modifica o tipo de dado da coluna 'preco' para DOUBLE PRECISION
-ALTER TABLE services ALTER COLUMN preco TYPE DOUBLE PRECISION;
